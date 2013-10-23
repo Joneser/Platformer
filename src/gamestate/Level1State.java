ec23cc558e56b15f265b331447f13811aab17611
David <David@David-PC.Home>
2013-10-23 21:52:56 +0100

    Added abstract entity class - MapObject
    Change-Id: Icd15cfe180e0835dddf8d5f5d12285f707ab69c2

--- src/gamestate/Level1State.java
+++ src/gamestate/Level1State.java
@@ -1,15 +1,14 @@
 package gamestate;
 
-import java.awt.Color;
 import java.awt.Graphics2D;
 
-import main.GamePanel;
-
+import tilemap.Background;
 import tilemap.TileMap;
 
 public class Level1State extends GameState {
 
 	private TileMap tileMap;
+	private Background bg;
 	
 	public Level1State(GameStateManager gsm) {
 		this.gsm = gsm;
@@ -21,6 +20,8 @@ public class Level1State extends GameState {
 		tileMap.loadTiles("/Tilesets/grasstileset.gif");
 		tileMap.loadMap("/Maps/level1-1.map");
 		tileMap.setPosition(0, 0);
+		
+		bg = new Background("/Backgrounds/grassbg1.gif", 0.1);
 	}
 	
 	public void update() {
@@ -28,9 +29,8 @@ public class Level1State extends GameState {
 	}
 	
 	public void draw(Graphics2D g) {
-		// clea screen
-		g.setColor(Color.WHITE);
-		g.fillRect(0, 0, GamePanel.WIDTH, GamePanel.HEIGHT);
+		// draw background
+		bg.draw(g);
 		
 		// draw tilemap
 		tileMap.draw(g);
